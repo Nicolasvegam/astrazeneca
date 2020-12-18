5da0022fa417008dcb5722a45289ef43675a54f2
fernanda <fvmansilla@uc.cl>
2020-12-18 00:31:41 -0300

    competencia principal- secundaria

--- static/app1/js/demo/combochart.js
+++ static/app1/js/demo/combochart.js
@@ -3,9 +3,6 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawVisualization);
 
-const principal= 'Conocimiento'
-var secundaria = 'Conocimiento del Cliente'
-
 const datos = [{
     'id':0,
     'nivel':'Regular',
@@ -87,33 +84,69 @@ const datos = [{
         'list': ['comportamiento 1','comportamiento 2','comportamiento 3','comportamiento 4'],
         'scores': [2,3,1,4]}],
 },
+{
+    'id':1,
+    'nivel':'Regular',
+    'nombre':'Conocimiento del Cliente',
+    'padre':'Conocimiento' ,
+    'comportamientos':[{
+        'list': ['comportamiento 1','comportamiento 2','comportamiento 3','comportamiento 4'],
+        'scores': [3,2,3,2]}],
+},
+{
+    'id':2,
+    'nivel':'Regular',
+    'nombre':'Conocimiento del Cliente',
+    'padre':'Conocimiento' ,
+    'comportamientos':[{
+        'list': ['comportamiento 1','comportamiento 2','comportamiento 3','comportamiento 4'],
+        'scores': [3,2,3,2]}],
+},
 ]
 
+const principal= 'Conocimiento'
+var secundaria = 'Conocimiento del Cliente'
+const id_usuario = 1
 
+
+
+function transpose(a) {
+    return Object.keys(a[0]).map(function(c) {
+        return a.map(function(r) { return r[c]; });
+    });
+}
 
 
 async function drawVisualization( ) {
     var cap_name = await document.getElementById("capacidad").value;
-    secundaria = cap_name
-    //console.log(cap_name)
-    // Para graficar secundarias
-    var comportamientos = await datos.filter(d => d.nombre == secundaria)[0].comportamientos[0].list
+    //obtengo lista de competencias secundarias de la competencia principal
+    //Para cada competencia secundaria
+    //Obtengo comportamientos de la competencia secundaria
+    var comportamientos = datos.filter(d => d.nombre == secundaria)[0].comportamientos[0].list
     var basico = datos.filter(d => d.nombre == secundaria && d.nivel == 'Basico')[0].comportamientos[0].scores
     var regular = datos.filter(d => d.nombre == secundaria && d.nivel == 'Regular')[0].comportamientos[0].scores
     var experto = datos.filter(d => d.nombre == secundaria && d.nivel == 'Experto')[0].comportamientos[0].scores
+    var real = datos.filter(d => d.nombre == secundaria  && d.id== id_usuario )[0].comportamientos[0].scores
+    //Obtengo puntaje de cada competencia secundaria a partir de sus compartamientos
+    //Adapto datos a formato del grafico
+    var capacidades_secundarias = ['Experticia','Conocimiento del Cliente','Conocimiento del Entorno','Compliance']
+    var sf=['Fundamental',6,9,10]
+    var sr=['Regular',6,12,11]
+    var sp=['Profesional',12,12,14]
+    var sreal=['Real',8,7,12]
+    // Calcula con cual es más semejante (diferncia de vectores)
+    var categoria = 'Regular'
 
-    comportamientos.unshift('Experticia')
-    basico.unshift('Basico')
-    console.log(datos.filter(d => d.nombre == cap_name && d.nivel == 'Basico'))
-    regular.unshift('Regular')
-    experto.unshift('Experto')
 
-    var data = google.visualization.arrayToDataTable([comportamientos, basico, regular, experto])
+    datos_finales = [capacidades_secundarias, sf, sr, sp, sreal]
+    datos_transpuestos = transpose(datos_finales)
+
+    var data = google.visualization.arrayToDataTable(datos_transpuestos)
 
     var options = {
-        title : cap_name,
+        title : secundaria,
         vAxis: {title: 'Nivel requerido'},
-        hAxis: {title: 'Experticia'},
+        hAxis: {title: 'Competencias secundarias'},
         seriesType: 'bars',
         series: {5: {type: 'line'}}
     };
