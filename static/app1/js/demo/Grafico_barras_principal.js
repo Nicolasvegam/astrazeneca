b0c5c155d2aa28404d1efe79657f414db8ae9dd8
Nicolasvegam <nicolasvega@ip-192-168-0-130.sa-east-1.compute.internal>
2020-12-20 13:57:16 -0300

    conexion

--- static/app1/js/demo/Grafico_barras_principal.js
+++ static/app1/js/demo/Grafico_barras_principal.js
@@ -121,6 +121,9 @@ function transpose(a) {
 async function drawVisualization( ) {
     //Se obtiene Data desde vista
     var cap_name = await document.getElementById("capacidad").value;
+    var appData = await document.getElementById("otro").value;
+    
+    console.log(appData)
     const id_usuario = 1
     //obtengo lista de competencias secundarias de la competencia principal
     //Para cada competencia secundaria
