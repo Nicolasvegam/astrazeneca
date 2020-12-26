4809dc0ce2c01c8db3c8b26e432eb6d3ee7b398e
fernanda <fvmansilla@uc.cl>
2020-12-25 21:52:46 -0300

    Responsive Google chart, 2row Comportamientos, Calculo categoria, +otros

--- static/app1/js/demo/Grafico_barras_principal.js
+++ static/app1/js/demo/Grafico_barras_principal.js
@@ -50,13 +50,29 @@ async function getData(){
         var scores = scores_acum.map(score => score/ scores_list.length).reduce((a,b) => a+b, 0) //Se divide en numero de individuos y despues se suman
         scores_real.push(scores)
     })
+    //----CALCULO DE CATEGORÍA------
+    var categoria = 'Fundamental'
+    var score_obtenido = scores_real.reduce((a,b) => a+b, 0)
+    var umbral_regular = scores_regular.reduce((a,b) => a+b, 0)
+    var umbral_profesional = scores_profesional.reduce((a,b) => a+b, 0)
+    if (score_obtenido < umbral_regular){
+        categoria = 'Fundamental'
+    }
+    else if (score_obtenido < umbral_profesional){
+        categoria = 'Regular'
+    }
+    else {
+        categoria='Profesional'
+    }
+    console.log('CATEGORIA', categoria, [score_obtenido, umbral_regular, umbral_profesional])
+    
      //Adapto datos a formato del grafico
     capacidades_secundarias.unshift('Experticia')
     scores_fundamental.unshift('Fundamental')
     scores_regular.unshift('Regular')
     scores_profesional.unshift('Profesional')
     scores_real.unshift('Real')
-    return [capacidades_secundarias, scores_fundamental, scores_regular, scores_profesional, scores_real] 
+    return {'data':[capacidades_secundarias, scores_fundamental, scores_regular, scores_profesional, scores_real],'categoria':categoria} 
   
 }
 
@@ -65,23 +81,36 @@ async function drawVisualization( ) {
     //Nombre Grafico
     var cap_name = await document.getElementById("capacidad").value;
 
-    // Calcula con cual es más semejante (diferncia de vectores)
-    var categoria = 'Regular'
-
+    //Datos
     var datos_finales = await  getData()
-    datos_transpuestos = transpose(datos_finales)
+    document.getElementById('categoria').innerHTML = datos_finales.categoria;
+
+    datos_transpuestos = transpose(datos_finales.data)
 
     var data = google.visualization.arrayToDataTable(datos_transpuestos)
 
     var options = {
         title : cap_name,
         vAxis: {title: 'Nivel '},
-        hAxis: {title: 'Competencias secundarias',slantedText: true, slantedTextAngle: 0},
+        hAxis: {title: 'Competencias secundarias',slantedText: true,
+         slantedTextAngle: 30,
+         textStyle: {'fontSize': 10},
+         titleTextStyle: {
+            fontSize: 12, 
+            bold: 'true'
+        },
+        format: '####'},
         seriesType: 'bars',
         series: {5: {type: 'line'}}
     };
 
   var chart = new google.visualization.ComboChart(document.getElementById('chart_div2'));
-  chart.draw(data, options);
+    chart.draw(data, options);
+    window.onresize = doALoadOfStuff;
+
+    function doALoadOfStuff() {
+        //do a load of stuff
+        chart.draw(data, options);
+    }
 
 }
